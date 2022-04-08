b42bf87a715ee3da16bd3c1ffcb5886ef5c64e47
AMIT KUMAR YADAV <55694729+MrAmit-itech@users.noreply.github.com>
2022-04-08 10:45:31 +0530

    updates

--- react_assign/src/Components/signup.jsx
+++ react_assign/src/Components/signup.jsx
@@ -2,6 +2,9 @@
 import {Link} from "react-router-dom"
 import { useState } from "react"
 
+
+
+
 const SignUp=()=>{
     const[data,setData] = useState({})
     const[passwarn,setPasswarn] = useState(null)
